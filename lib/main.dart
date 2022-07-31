c91ffcff17b0cb7da68abf435e69b420d3524e27
L3odr0id <leon.leonc@yandex.ru>
2022-07-31 20:43:51 +0300

    Delete old FFI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:ffi';
+import 'dart:io';
+
 import 'package:easy_localization/easy_localization.dart';
+import 'package:ffi/ffi.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:threedpass/core/persistence/hive_setup.dart' as hive_setup;
@@ -6,6 +10,7 @@ import 'package:threedpass/core/polkawallet/bloc/app_service_cubit.dart';
 import 'package:threedpass/features/hashes_list/bloc/hashes_list_bloc.dart';
 import 'package:threedpass/features/settings_page/bloc/settings_page_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:threedpass/generated/bindings.dart';
 import 'package:threedpass/router/router.gr.dart';
 import 'package:threedpass/setup.dart' as di_setup;
 
@@ -34,12 +39,40 @@ class ThreeDApp extends StatelessWidget {
 
   final _appRouter = AppRouter();
 
+  void testLib() {
+    final DynamicLibrary nativeExampleLib = Platform.isAndroid
+        ? DynamicLibrary.open(
+            "libcalc.so") // Load the dynamic library on Android
+        : DynamicLibrary.process(); // Load the static library on iOS
+
+    // final bindings = GreeterBindings(nativeExampleLib);
+
+    final Pointer<Utf8> Function(Pointer<Utf8>) rustGreeting = nativeExampleLib
+        .lookup<NativeFunction<Pointer<Utf8> Function(Pointer<Utf8>)>>(
+            "rust_greeting")
+        .asFunction();
+
+    // Prepare the parameters
+    final name = "John Smith";
+    final Pointer<Utf8> namePtr = name.toNativeUtf8();
+    print("- Calling rust_greeting with argument:  $namePtr");
+
+// Call rust_greeting
+    final Pointer<Utf8> resultPtr = rustGreeting(namePtr);
+    print("- Result pointer:  $resultPtr");
+
+    final String greetingStr = resultPtr.toDartString();
+    print("- Response string:  $greetingStr");
+  }
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitDown,
       DeviceOrientation.portraitUp,
     ]);
+
+    testLib();
 
     return MultiBlocProvider(
       providers: [
---
+++ rust/target/bindings.h
@@ -0,0 +1,3 @@
+// NOTE: Append the lines below to ios/Classes/<your>Plugin.h
+
+char *rust_greeting(const char *to);
