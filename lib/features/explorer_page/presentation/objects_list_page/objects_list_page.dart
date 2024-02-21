02a577a26207468afd53f75631917c17adabf0b5
L3odr0id <leon.leonc@yandex.ru>
2024-02-21 18:30:10 +0300

    Show hashes in uploaded object page

--- lib/features/explorer_page/presentation/objects_list_page/objects_list_page.dart
+++ lib/features/explorer_page/presentation/objects_list_page/objects_list_page.dart
@@ -1,12 +1,8 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:threedpass/core/widgets/paddings.dart';
 import 'package:threedpass/features/explorer_page/presentation/objects_list_page/objects_list_appbar.dart';
-import 'package:threedpass/features/explorer_page/presentation/objects_list_page/reload_objects_icon_button.dart';
-import 'package:threedpass/features/poscan_objects_query/bloc/poscan_objects_cubit.dart';
 import 'package:threedpass/features/wallet_screen/assets_page/widgets/objects_list/objects_list.dart';
 
 @RoutePage()
--- lib/features/uploaded_object_page/uploaded_object_page.dart
+++ lib/features/uploaded_object_page/uploaded_object_page.dart
@@ -1,7 +1,9 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:threedpass/core/utils/formatters.dart';
 import 'package:threedpass/core/widgets/d3p_scaffold.dart';
+import 'package:threedpass/core/widgets/other/fast_rich_text.dart';
 import 'package:threedpass/core/widgets/paddings.dart';
 import 'package:threedpass/core/widgets/utc_time.dart';
 import 'package:threedpass/features/poscan_objects_query/domain/entities/uploaded_object.dart';
@@ -20,36 +22,39 @@ class UploadedObjectPage extends StatelessWidget {
   Widget build(final BuildContext context) {
     return D3pScaffold(
       appbarTitle: 'uploaded_object_title',
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        // crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBoxH16(),
+          UploadedObjectIdText(uploadedObject: uploadedObject),
+          const SizedBoxH16(),
+          Row(
             children: [
-              const SizedBoxH16(),
-              UploadedObjectIdText(uploadedObject: uploadedObject),
-              Row(
-                children: [
-                  UploadedObjectStatusText(uploadedObject: uploadedObject),
-                  const SizedBox(width: 16),
-                  UTCTime(
-                    dateTime: uploadedObject.statusDateUTC,
-                    formatter: Fmt.shortDateFormat,
-                  ),
-                ],
+              UploadedObjectStatusText(uploadedObject: uploadedObject),
+              const SizedBox(width: 16),
+              UTCTime(
+                dateTime: uploadedObject.statusDateUTC,
+                formatter: Fmt.shortDateFormat,
               ),
-              UploadedObjectOwnerText(uploadedObject: uploadedObject),
-              SnapshotConnectedToUploaded(
-                uploadedObject: uploadedObject,
-                topPadding: 16,
-              ),
-
-              // const SizedBoxH24(),
-              // const D3pBodyMediumText('Raw:'), // TODO LOCALIZE
-              // RawUploadedObjectContent(uploadedObject: uploadedObject),
             ],
           ),
-        ),
+          const SizedBoxH16(),
+          UploadedObjectOwnerText(uploadedObject: uploadedObject),
+          SnapshotConnectedToUploaded(
+            uploadedObject: uploadedObject,
+            topPadding: 16,
+          ),
+          const SizedBoxH16(),
+          FastRichText(
+            mainText: '\n' + uploadedObject.hashes.join('\n'),
+            secondaryText: 'uploaded_object_hashes'.tr(),
+          ),
+
+          // const SizedBoxH24(),
+          // const D3pBodyMediumText('Raw:'), // TODO LOCALIZE
+          // RawUploadedObjectContent(uploadedObject: uploadedObject),
+        ],
       ),
     );
   }
